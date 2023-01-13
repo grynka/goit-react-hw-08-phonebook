7b52fb149a9b1a13f696b3c17b20bc721324e700
grynka <ihor.kozhemyakin@gmail.com>
2023-01-13 19:18:55 +0200

    upd react

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from 'components/App';
 import { Provider } from 'react-redux';
 import  { store }  from './redux/store';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <React.StrictMode>
     <BrowserRouter basename='goit-react-hw-08-phonebook'>
@@ -14,8 +14,5 @@ ReactDOM.render(
       <App />
       </BrowserRouter>
     </React.StrictMode>
-  </Provider>,
-  
-  document.getElementById('root')
+  </Provider>
 );
-
