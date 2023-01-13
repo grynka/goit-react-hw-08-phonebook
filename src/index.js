e4f143af4615a485ec1fee93732f041aa87d9a00
grynka <ihor.kozhemyakin@gmail.com>
2023-01-13 19:08:22 +0200

    fix conflict

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from 'components/App';
 import { Provider } from 'react-redux';
 import  { store }  from './redux/store';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
     <BrowserRouter basename='goit-react-hw-08-phonebook'>
@@ -14,5 +14,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <App />
       </BrowserRouter>
     </React.StrictMode>
-  </Provider>
+  </Provider>,
+  
+  document.getElementById('root')
 );
+
