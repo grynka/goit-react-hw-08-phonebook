10bc7ccacf71ea63a6ea823d5f7e2f3cc53c2e4c
grynka <ihor.kozhemyakin@gmail.com>
2023-06-22 22:17:34 +0300

    upd

--- src/redux/auth/operatons.js
+++ src/redux/auth/operatons.js
@@ -2,7 +2,7 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 
-axios.defaults.baseURL = 'https://connections-api.herokuapp.com/';
+axios.defaults.baseURL = 'https://hwt.onrender.com';
 
 const setAuthHeader = token => {
   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
